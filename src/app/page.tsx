cc2f7ec6dd6ccceee0ed6a9131f650c84d7b8af6
lakshya-roonwal <erenpool7214@gmail.com>
2024-01-11 23:50:37 +0530

    Added Download and copy Reamde button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="container">
-    <h1 className='text-3xl font-bold text-center'>Hello World</h1>
-    <div className='w-full flex justify-center p-4'>
-    <Button asChild>
-      <Link href={'/editor'}>
-        Get Started
-      </Link>
+    <section className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-800 dark:text-gray-100 mb-6">
+        Welcome to Readme Generator
+      </h1>
+      <p className="text-lg md:text-xl lg:text-2xl text-center text-gray-600 dark:text-gray-300 mb-10 px-4">
+        Create professional and beautiful readme files for your projects in just
+        a few clicks.
+      </p>
+      <Button asChild>
+        <Link href={"/editor"}>Get Started</Link>
       </Button>
-    </div>
-    </div>
-  )
+    </section>
+  );
 }
--- src/components/editor/MainSection.tsx
+++ src/components/editor/MainSection.tsx
@@ -5,15 +5,39 @@ import "github-markdown-css"; // Import GitHub Markdown CSS
 import remarkGfm from "remark-gfm";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LuClipboard, LuClipboardCheck } from "react-icons/lu";
+import { MdOutlineFileDownload } from "react-icons/md";
+
 
 const MainSection = ({readmeSections}:any) => {
 
   const [readme, setReadme] = useState('')
+  const [copy, setcopy] = useState(false);
+
+  const handleDownload=()=>{
+    const blob = new Blob([readme], { type: "text/markdown" });
+
+    // Create a download link
+    const downloadLink = document.createElement("a");
+
+    // Set the download link's href to a URL created from the Blob
+    downloadLink.href = URL.createObjectURL(blob);
+
+    // Set the file name for the download
+    downloadLink.download = "Readme.md";
+
+    // Append the link to the document
+    document.body.appendChild(downloadLink);
+
+    // Trigger a click on the link to start the download
+    downloadLink.click();
+
+    // Remove the link from the document
+    document.body.removeChild(downloadLink);
+
+  }
 
   useEffect(()=>{
-    // setReadme(()=>{
-    //   return readmeSections.map((e)=>{e.markdown})
-    // })
     const concatenatedMarkdown = readmeSections
       .filter((section) => section.inReadme)
       .map((section) => section.markdown)
@@ -27,13 +51,19 @@ const MainSection = ({readmeSections}:any) => {
         <div className="flex justify-between mb-4">
           <h2 className="text-xl font-semibold text-gray-800">Preview</h2>
           <div className="flex space-x-2">
+            <Button variant="default" onClick={handleDownload}>
+              <MdOutlineFileDownload className="mr-2 h-6 w-6" />
+              Download
+            </Button>
             <TabsList className="grid w-full grid-cols-2">
               <TabsTrigger value="preview">Preview</TabsTrigger>
               <TabsTrigger value="raw">Raw</TabsTrigger>
             </TabsList>
           </div>
         </div>
-        <div className="max-h-screen">
+        <div
+          className="max-h-screen"
+        >
           <TabsContent value="preview">
             <div className="markdown-body">
               <ReactMarkdown
@@ -43,12 +73,40 @@ const MainSection = ({readmeSections}:any) => {
               />
             </div>
           </TabsContent>
-          <TabsContent value="raw" className="h-[calc(100vh-150px)]">
-            <textarea
-              readOnly
-              className="h-full w-full border-1  resize-none bg-white dark:bg-gray-800 focus:outline-none"
-              value={readme}
-            ></textarea>
+          <TabsContent value="raw" className="">
+            <div className="flex flex-col ">
+              <div className="w-full flex justify-end px-4">
+                {copy ? (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="py-1 inline-flex items-center gap-1"
+                  >
+                    <LuClipboardCheck size={"18px"} />
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="py-1 inline-flex items-center gap-1"
+                    onClick={() => {
+                      navigator.clipboard.writeText(readme);
+                      setcopy(true);
+                      setTimeout(() => {
+                        setcopy(false);
+                      }, 3000);
+                    }}
+                  >
+                    <LuClipboard size={"18px"} />
+                  </Button>
+                )}
+              </div>
+              <textarea
+                readOnly
+                className="h-[calc(100vh-150px)] w-full border-1  resize-none bg-white dark:bg-gray-800 focus:outline-none"
+                value={readme}
+              ></textarea>
+            </div>
           </TabsContent>
         </div>
       </Tabs>
