c7cacb07c9386776ef88289f7b2e2f6321ab37c6
lakshya-roonwal <erenpool7214@gmail.com>
2024-01-30 22:28:58 +0530

    favion added

--- src/components/editor/ReadmeEditor.tsx
+++ src/components/editor/ReadmeEditor.tsx
@@ -1,34 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
-import Editor from "@monaco-editor/react";
-import MarkdownEditor from './MarkdownEditor';
+import { useEffect, useState } from "react";
+import MarkdownEditor from "./MarkdownEditor";
 
 const ReadmeEditor = ({ readmeSections, setReadmeSections }: any) => {
+  const [readmeMarkdown, setReadmeMarkdown] = useState<string | null>(null);
+  useEffect(() => {
+    // Find the first object with selected set to true
+    const selectedObject = readmeSections.find((section) => section.selected);
 
-   const [readmeMarkdown, setReadmeMarkdown] = useState<string | null>(null);
+    // Set the selected section state
+    setReadmeMarkdown((prevMarkdown) => selectedObject || null);
+  }, [readmeSections]);
 
-   useEffect(() => {
-     // Find the first object with selected set to true
-     const selectedObject = readmeSections.find((section) => section.selected);
-
-     // Set the selected section state
-     setReadmeMarkdown((prevMarkdown) => selectedObject || null);
-   }, [readmeSections]);
-
-   console.log(readmeMarkdown);
+  console.log(readmeMarkdown);
 
   const handleTextareaChange = (markdown: string) => {
-        setReadmeSections((prevElements) =>
-          prevElements.map((element) =>
-            element.id === readmeMarkdown.id
-              ? { ...element, ["markdown"]: markdown }
-              : element
-          )
-        );
+    setReadmeSections((prevElements) =>
+      prevElements.map((element) =>
+        element.id === readmeMarkdown.id
+          ? { ...element, ["markdown"]: markdown }
+          : element
+      )
+    );
   };
   return (
     <section className="sm:w-1/2 bg-gray-700 p-6 h-[50vh] sm:h-screen">
@@ -36,23 +28,22 @@ const ReadmeEditor = ({ readmeSections, setReadmeSections }: any) => {
         <div className="">
           <div className="flex justify-between mb-4">
             <h2 className="text-xl font-semibold text-white">Editor</h2>
-            <div className="flex space-x-2">
-            </div>
+            <div className="flex space-x-2"></div>
           </div>
-            <div className='h-[30vh] sm:h-[calc(100vh-100px)] overflow-hidden'>
+          <div className="h-[30vh] sm:h-[calc(100vh-100px)] overflow-hidden">
             <MarkdownEditor
               readme={readmeMarkdown?.markdown}
               handleTextareaChange={handleTextareaChange}
-              />
-              </div>
+            />
           </div>
+        </div>
       ) : (
         <div className="w-full border-1 text-center items-center p-4">
-          <p className='text-white'>Select a section from sidebar to edit</p>
+          <p className="text-white">Select a section from sidebar to edit</p>
         </div>
       )}
     </section>
   );
 };
 
-export default ReadmeEditor
+export default ReadmeEditor;
